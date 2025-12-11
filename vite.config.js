cb52d0b68650d8531fe887124a77ca6c82186b17
Hana250903 <kiritomiku0@gmail.com>
2025-12-11 15:41:53 +0700

    fix: deploy

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Base URL cho GitHub Pages - thay 'front-end-demo' bằng tên repo của bạn
-  base: '/front-end-demo/',
+  // Base URL cho GitHub Pages
+  base: '/web-demo/',
 })
